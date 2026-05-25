e18614af24e060d62563cd55e8219c41d6ca8204
umi6 <124774831+umi6@users.noreply.github.com>
2026-05-25 17:41:28 +0900

    p

--- KyuugiAPI/vite.config.ts
+++ KyuugiAPI/vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_PATH ?? '/kyuugiAPI/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
